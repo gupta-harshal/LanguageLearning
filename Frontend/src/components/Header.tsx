df1151aa5d701cac2a48f9e94ca6fa781ce0f0d6
Aakri1134 <aakrisri55@gmail.com>
2025-07-18 11:25:11 +0530

    Update Home

---
+++ Frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header () {
+
+    return(
+        <div className=" absolute bg-primary-background/25 top-0 left-0 w-full h-20 flex  z-10 shadow-lg ">
+            <div className=" "></div>
+        </div>
+    )
+}
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,25 +1,27 @@
+import Header from "../components/Header"
 import Button from "../components/Home/Button"
 import PrintingText from "../components/PrintingText"
 
 export default function Home() {
   return (
     <div className="light bg-gradient-to-b h-[2000px] from-primary-foreground via-white to-primary-button ">
-      <div className=" h-screen flex flex-row">
-        <div className=" w-96">
+        <Header/>
+      <div className=" h-screen flex flex-row justify-center items-center gap-20">
+        <div className=" w-[625px] h-80">
           <PrintingText
             texts={[
-              "Learn Japanese the right way!!!",
-              "正しい方法で日本語を学びましょう!!!",
+              "Learn Japanese the right way !!!",
+              "正しい 方法 で 日本語 を 学びましょう!!!",
             ]}
             className={[
-              " font-anglo-japanese text-white text-6xl",
-              " font-japanese text-white text-6xl",
+              " font-anglo-japanese text-white text-8xl text-shadow-[5px_5px_1px] text-shadow-black/40 ",
+              " font-japanese text-white text-8xl text-shadow-[5px_5px_1px] text-shadow-black/40",
             ]}
           />
         </div>
         <div className=" flex flex-col gap-7">
             <Button className=" bg-primary-button shadow-[#ff2054]">Get Started</Button>
-            <Button className=" bg-secodary-button shadow-[#ff688b]">Get Started</Button>
+            <Button className=" bg-secodary-button shadow-[#ff688b]">Already A Member</Button>
         </div>
       </div>
     </div>

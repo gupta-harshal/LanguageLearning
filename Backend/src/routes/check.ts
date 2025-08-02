71770bdbd43cab6d566f8a0db7788cee85bb7c3f
gupta-harshal <harshal.2023ug3013@iiitranchi.ac.in>
2025-08-02 21:31:57 +0530

    fix

--- Backend/src/routes/check.ts
+++ Backend/src/routes/check.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import grammarEngine from "../apis/GrammarEngine";
+import { authenticate } from "../middlewares/authentication";
 const checkRouter= Router();
-checkRouter.post('/check',grammarEngine);
+checkRouter.post('/check',authenticate,grammarEngine);
 export default checkRouter;

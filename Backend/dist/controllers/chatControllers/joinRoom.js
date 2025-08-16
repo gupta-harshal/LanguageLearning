f5f6103f596dea4e7adbdba8744e87df41f955ee
gupta-harshal <harshal.2023ug3013@iiitranchi.ac.in>
2025-08-16 23:25:46 +0530

    fix

--- Backend/dist/controllers/chatControllers/joinRoom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.joinRoomWithoutID = exports.joinRoomWithID = void 0;
-const joinRoomWithID = (req, res) => { };
-exports.joinRoomWithID = joinRoomWithID;
-const joinRoomWithoutID = (req, res) => {
-    const { user } = req;
-};
-exports.joinRoomWithoutID = joinRoomWithoutID;
--- Backend/dist/utils/redis.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redis = void 0;
-const redis_1 = require("@upstash/redis");
-exports.redis = new redis_1.Redis({
-    url: 'https://whole-skylark-6474.upstash.io',
-    token: process.env.REDIS_TOKEN,
-});
